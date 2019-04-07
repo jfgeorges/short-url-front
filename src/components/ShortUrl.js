daf4ec0c0ee93c17a331790b05c32d60d88959a1
Jeff <jeffosten@gmail.com>
2019-04-07 23:29:42 +0200

    Cursor on links

--- src/components/ShortUrl.js
+++ src/components/ShortUrl.js
@@ -3,14 +3,14 @@ import React from "react";
 const ShortUrl = props => {
   return (
     <>
-      <div className="originalUrl" onClick={() => props.handleUrlClick(props.index, props.shortUrl)}>
+      <div className="originalUrl link" onClick={() => props.handleUrlClick(props.index, props.shortUrl)}>
         {props.originalUrl}
       </div>
-      <div className="shortUrl" onClick={() => props.handleUrlClick(props.index, props.shortUrl)}>
+      <div className="shortUrl link" onClick={() => props.handleUrlClick(props.index, props.shortUrl)}>
         {props.api_url + props.shortUrl}
       </div>
       <div className="visits bgcolor">
-        {props.visitCounter} <i className="far fa-trash-alt" onClick={() => props.handleDeleteUrl(props.shortUrl)} />
+        {props.visitCounter} <i className="far fa-trash-alt link" onClick={() => props.handleDeleteUrl(props.shortUrl)} />
       </div>
       <div className="bottom-line" />
     </>
